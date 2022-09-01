8719cd4d2535ad85c38ea84ac47ceff632f2d119
Tommy Praeger <thomaspraeger2020@u.northwestern.edu>
2022-09-01 14:17:36 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
-import express from 'express';
-
+const express = require('express');
 const app = express();
+const process = require('process')
+const exec = require('child_process').exec;
 
-app.get('/', function (request, response) {
-    response.send(`node.js version is ${process.version}`);
+var node_version;
+
+exec("node -v", function (error, stdout, stderr) {
+    node_version = stdout.trim();
 });
 
-var port = process.env.PORT || 8080;
+app.get('/', function (request, response) {
+    var output = {'app_name': "Hello Express", 'node_version': node_version, 'env_vars': process.env};
+    response.send(output);
+});
+
+var port = process.env.PORT || 8000;
 app.listen(port, function () {
     console.log("Listening on " + port);
 });
